aaca855d319e00522c739f9abb8efbcdf7aaae86
bimmerr2019 <bimmer@Eros-3>
2021-06-23 14:22:48 -0700

    add zathura and newsboat

--- i3/scripts/random_wall2.sh
+++ i3/scripts/random_wall2.sh
@@ -4,5 +4,6 @@ wallpaperdir="$HOME/dotfiles/i3/wallpapers"
 files=($wallpaperdir/*)
 randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"`
 
-cp $randompic $HOME/dotfiles/i3/feh/wallpaper.jpg
+cp $randompic $HOME/.config/i3/feh/wallpaper.jpg
+feh --no-fehbg --bg-fill $HOME/.config/i3/feh/wallpaper.jpg
 
